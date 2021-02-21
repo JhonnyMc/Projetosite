c7ffd05760e15dd3d54124d1cd7205e69ead9057
wend3ll-souza <wend3llsouza@gmail.com>
2021-02-21 13:06:36 -0300

    :v: bobDevelop data.js e forIn prontos

---
+++ data.js
@@ -0,0 +1,26 @@
+
+const data = [
+    {
+        titulo: "Titulo",
+        describe: "descrição da do card",
+        imagePath: "https://source.unsplash.com/random",
+    },
+
+    {
+        titulo: "Titulo",
+        describe: "descrição da do card",
+        imagePath: "https://source.unsplash.com/random",
+    },
+
+    {
+        titulo: "Titulo",
+        describe: "descrição da do card",
+        imagePath: "https://images.pexels.com/photos/6553054/pexels-photo-6553054.jpeg?cs=srgb&dl=pexels-lisa-fotios-6553054.jpg&fm=jpg",
+    },
+
+    {
+        titulo: "Titulo",
+        describe: "descrição da do card",
+        imagePath: "https://source.unsplash.com/random",
+    },
+];
--- script.js
+++ script.js
@@ -1,36 +1,56 @@
+// Syntax 
 
-// Selecinando Elemento HTML
-const sessaoCards = document.getElementById('sectionCards');
+// FUNCOES
+/*
+    function nomeDaFuncao (pametros){
+        bloco de codigo
+    }
+*/
 
-/*Criando ELementos */
-const image = document.createElement('img');
-const titulo = document.createElement('h2');
-const paragrafo = document.createElement('p');
-const div = document.createElement('div');
+// LOOPS
+/* for (index= 0; index < 10; index++) {}
+    for (index in array);
+*/
+
+// Função que cria card
+function createCard(imagPath, describe, title) {
+
+    // Selecinando Elemento HTML
+    const sessaoCards = document.getElementById('sectionCards');
+    
+    /*Criando ELementos */
+    const image = document.createElement('img');
+    const titulo = document.createElement('h2');
+    const paragrafo = document.createElement('p');
+    const div = document.createElement('div');
+    
+    // adicionando Classes
+    div.className = 'cardDescribe';
+    image.className = 'Imagecontainer';
+    
+    // Adicionando texto
+    titulo.innerText = title;
+    paragrafo.innerText = describe;
+    
+    // Caminho para foto;
+    image.setAttribute("src", imagPath);
+
+    // Adicionando paragrafo ao elemento ancora
+    div.appendChild(titulo)
+    div.appendChild(paragrafo);
+    sessaoCards.appendChild(div);
+    sessaoCards.appendChild(image);
+
+}
+
+
+// Lopp que esta renderizando cards com dados do Data,js
+for (index in data) {
+    createCard( data[index].imagePath, data[index].describe, data[index].titulo )
+}
 
 
 
-// adicionando Classes
-div.className = 'cardDescribe';
-image.className = 'Imagecontainer';
-
-// Adicionando texto ao paragrafo
-titulo.innerText = 'Titulo'
-paragrafo.innerText = 'Olá mundo';
-
-
-
-
-// Caminho para foto;
-image.setAttribute("src", "https://source.unsplash.com/random");
-
-
-
-// Adicionando paragrafo ao elemento ancora
-div.appendChild(titulo)
-div.appendChild(paragrafo);
-sessaoCards.appendChild(div);
-sessaoCards.appendChild(image);
 
 
 
