d0068fd8d3dea7e9dcc5eb06ebca77bb74979a28
wend3ll-souza <wend3llsouza@gmail.com>
2021-02-21 13:41:10 -0300

    :v: bobDevelop css em andamento

--- data.js
+++ data.js
@@ -23,4 +23,27 @@ const data = [
         describe: "descrição da do card",
         imagePath: "https://source.unsplash.com/random",
     },
+    {
+        titulo: "Titulo",
+        describe: "descrição da do card",
+        imagePath: "https://source.unsplash.com/random",
+    },
+
+    {
+        titulo: "Titulo",
+        describe: "descrição da do card",
+        imagePath: "https://source.unsplash.com/random",
+    },
+
+    {
+        titulo: "Titulo",
+        describe: "descrição da do card",
+        imagePath: "https://images.pexels.com/photos/6553054/pexels-photo-6553054.jpeg?cs=srgb&dl=pexels-lisa-fotios-6553054.jpg&fm=jpg",
+    },
+
+    {
+        titulo: "Titulo",
+        describe: "descrição da do card",
+        imagePath: "https://source.unsplash.com/random",
+    },
 ];
--- script.js
+++ script.js
@@ -23,6 +23,7 @@ function createCard(imagPath, describe, title) {
     const titulo = document.createElement('h2');
     const paragrafo = document.createElement('p');
     const div = document.createElement('div');
+    const card = document.createElement('section');
     
     // adicionando Classes
     div.className = 'cardDescribe';
@@ -38,13 +39,14 @@ function createCard(imagPath, describe, title) {
     // Adicionando paragrafo ao elemento ancora
     div.appendChild(titulo)
     div.appendChild(paragrafo);
-    sessaoCards.appendChild(div);
-    sessaoCards.appendChild(image);
+    card.appendChild(div);
+    card.appendChild(image);
+    sessaoCards.appendChild(card);
 
 }
 
 
-// Lopp que esta renderizando cards com dados do Data,js
+// Loop que esta renderizando cards com dados do Data,js
 for (index in data) {
     createCard( data[index].imagePath, data[index].describe, data[index].titulo )
 }
